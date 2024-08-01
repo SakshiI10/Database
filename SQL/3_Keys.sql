224e654a1bb0b83edba05941f9177e7aefa84633
SakshiI10 <iamsakshiingole@gmail.com>
2024-08-01 16:33:06 +0530

    File added

---
+++ SQL/3_Keys.sql
@@ -0,0 +1,9 @@
+-- Keys
+
+-- 1. Primary Key:
+-- It is a column (or set of columns) in a table that uniquely identifies each row. (a unique id)
+-- There is only 1 PK & it should be NOT null.
+
+-- 2. Foreign Key
+-- A foreign key is a column (or set of columns) in a table that refers to the primary key in another table.
+-- FKs can have duplicate & null values
