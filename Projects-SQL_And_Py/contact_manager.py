9f3edfd7e6b9f65701905c465e4cdbef82d03cb7
SakshiI10 <iamsakshiingole@gmail.com>
2025-04-14 22:42:56 +0530

    file added

---
+++ Projects-SQL_And_Py/contact_manager.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",               
+    password="root123",   
+    database="contact_manager" 
+)
+
+cursor = conn.cursor()
+print("Connected to MySQL!")
+
+def add_contact():
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    email = input("Enter email: ")
+
+    query = "INSERT INTO contacts (name, phone_number, email) VALUES (%s, %s, %s)"
+    values = (name, phone, email)
+
+    cursor.execute(query, values)
+    conn.commit()
+
+    print("Contact added successfully!")
+
+add_contact()
+cursor.close()
+conn.close()
